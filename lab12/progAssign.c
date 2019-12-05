c85658856338bec511ddabd81398c344baa2add7
Filipe Castanheira <castanhf@arch06.cis.gvsu.edu>
2019-12-04 19:57:18 -0500

    no comments

--- lab12/progAssign.c
+++ lab12/progAssign.c
@@ -4,15 +4,11 @@
 #include <unistd.h>
 #include <errno.h>
 
-//#define SIZE 30
-
 int main (int argc, char *argv[]) {
     if (argc != 4) {
       printf("Usage:\n./a.out <link_type(h, s)> <target> <link_name>\n");
       exit(0);
     }
-
-    //printf("argv[0] = %s\nargv[1] = %s\targv[1][1] = %c\nargv[2] = %s\nargv[3] = %s\n", argv[0], argv[1], argv[1][1], argv[2], argv[3]);
 
     if (argv[1][0] == 'h') {
       if ((link(argv[2], argv[3])) < 0) {
